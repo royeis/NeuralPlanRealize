eb5e215bb3ede73e55b0ded6f7205a8496d6f463
Roy Eisenstadt <t-roye@microsoft.com>
2021-12-17 14:59:34 +0000

    Finished training notebook

--- Utils.py
+++ Utils.py
@@ -1,9 +1,16 @@
+import torch
+from torch.nn import CrossEntropyLoss
+
 def triple_to_string(triple):
     return '<S> ' + triple.subject + ' <P> ' + triple.predicate + ' <O> ' + triple.object
 
 def tripleset_to_string(tripleset):
     triple_strings = [triple_to_string(t) for t in tripleset]
     return ' '.join(triple_strings)
+
+def tripleset_map(tripleset):
+    triple_strings = [triple_to_string(t) for t in tripleset]
+    return {i: triple for i, triple in enumerate(triple_strings)}
 
 def get_plan(original_tripleset, ordered_tripleset):
     triple_strings = [triple_to_string(t) for t in original_tripleset]
@@ -35,10 +42,66 @@ def deepnlg_entry_to_examples(entry):
         example['category'] = entry.category
         example['eid'] = entry.eid
         example['size'] = entry.size
+        example['triples_map'] = tripleset_map(entry.modifiedtripleset)
         example['planner_input'] = tripleset_to_string(entry.modifiedtripleset)
         example['lid'] = lex.lid
         example['text'] = lex.text
         example['plan'] = get_plan(entry.modifiedtripleset, lex.orderedtripleset)
         example['realizer_input'] = ordered_tripleset_to_string(lex.orderedtripleset)
         examples.append(example)
+
     return examples
+
+class PlannerDataset(torch.utils.data.Dataset):
+    def __init__(self, examples, tokenizer):
+        self.tokenized_input = tokenizer([ex['planner_input'] for ex in examples], padding=True, return_tensors='pt')
+        self.labels = tokenizer([ex['plan'] for ex in examples], padding=True, return_tensors='pt')
+
+    def __getitem__(self, idx):
+        return {
+                'input_ids': self.tokenized_input['input_ids'][idx],
+                'attention_mask': self.tokenized_input['attention_mask'][idx],
+                'labels': self.labels['input_ids'][idx],
+                'decoder_attention_mask': self.labels['attention_mask'][idx]
+                }
+
+    def __len__(self):
+        return len(self.tokenized_input['input_ids'])
+
+
+class RealizerDataset(torch.utils.data.Dataset):
+    def __init__(self, examples, tokenizer):
+        self.tokenized_input = tokenizer([ex['realizer_input'] for ex in examples], padding=True, return_tensors='pt')
+        self.labels = tokenizer([ex['text'] for ex in examples], padding=True, return_tensors='pt')
+
+    def __getitem__(self, idx):
+        return {
+                'input_ids': self.tokenized_input['input_ids'][idx],
+                'attention_mask': self.tokenized_input['attention_mask'][idx],
+                'labels': self.labels['input_ids'][idx],
+                'decoder_attention_mask': self.labels['attention_mask'][idx]
+                }
+
+    def __len__(self):
+        return len(self.tokenized_input['input_ids'])
+
+
+
+def T2TDataCollator(batch):
+    """
+    Take a list of samples from a Dataset and collate them into a batch.
+    Returns:
+        A dictionary of tensors
+    """
+    input_ids = torch.stack([example['input_ids'] for example in batch])
+    lm_labels = torch.stack([example['labels'] for example in batch])
+    lm_labels[lm_labels[:, :] == 0] = -100
+    attention_mask = torch.stack([example['attention_mask'] for example in batch])
+    decoder_attention_mask = torch.stack([example['decoder_attention_mask'] for example in batch])
+
+    return {
+        'input_ids': input_ids,
+        'attention_mask': attention_mask,
+        'labels': lm_labels,
+        'decoder_attention_mask': decoder_attention_mask
+    }
